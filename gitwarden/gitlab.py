853261581953a9b2bf2e215f90d8022a88e505e0
Ellis Broatch <ellis.broatch@gmail.com>
2025-12-08 14:57:25 +0000

    per-project clone progress added

--- gitwarden/gitlab.py
+++ gitwarden/gitlab.py
@@ -15,43 +15,67 @@ import rich.progress
 import rich.table
 
 
-# # 1. Define the known operation names
-# OP_CODES = [
-#     "BEGIN",
-#     "CHECKING_OUT",
-#     "COMPRESSING",
-#     "COUNTING",
-#     "END",
-#     "FINDING_SOURCES",
-#     "RECEIVING",
-#     "RESOLVING",
-#     "WRITING",
-# ]
+# 1. Define the known operation names
+OP_CODES = [
+    "BEGIN",
+    "CHECKING_OUT",
+    "COMPRESSING",
+    "COUNTING",
+    "END",
+    "FINDING_SOURCES",
+    "RECEIVING",
+    "RESOLVING",
+    "WRITING",
+]
 
-# # 2. Build the map using a dictionary comprehension
-# # It maps the integer value to its string name
-# OP_CODE_MAP = {
-#     getattr(git.RemoteProgress, op_code): op_code
-#     for op_code in OP_CODES
-# }
+# 2. Build the map using a dictionary comprehension
+# It maps the integer value to its string name
+OP_CODE_MAP = {
+    getattr(git.RemoteProgress, op_code): op_code
+    for op_code in OP_CODES
+}
 
 
-PROGRESS = rich.progress.Progress(
-    # rich.progress.TextColumn("[bold blue]{task.fields[filename]}[/]"),
+PROGRESS_TOTAL = rich.progress.Progress(
+    rich.progress.TextColumn("Project: [progress.description]{task.description:30}"),
     rich.progress.BarColumn(),
     rich.progress.MofNCompleteColumn(),
 )
-TASK = PROGRESS.add_task(description="Processing", total=0)
+PROGRESS_PROJECT = rich.progress.Progress(
+    rich.progress.TextColumn("Step:    [progress.description]{task.description:30}"),
+    rich.progress.BarColumn(),
+    rich.progress.TextColumn("[green][progress.completed]{task.completed}%"),
+)
+TASK_TOTAL = PROGRESS_TOTAL.add_task(description="", total=0)
+TASK_PROJECT = PROGRESS_PROJECT.add_task(description="", total=100)
 TABLE = rich.table.Table()
 CONSOLE = rich.console.Console()
 LIVE = rich.live.Live(console=CONSOLE, refresh_per_second=10)
 LIVE.start()
 
-# class CloneProgress(git.RemoteProgress):
-#     def update(self, op_code, cur_count, max_count=None, message=''):
-#         # print(self.get_curr_op(op_code))
-#         OP_CODE_MAP.get(op_code)
-#         cur_count, max_count
+
+class CloneProgress(git.RemoteProgress):
+    """Override git.RemoteProgress to update progress bars."""
+    last_op_code = None
+
+    def update(self, op_code, cur_count, max_count=None, message=''):
+        """Get updates from git, pass to rich output."""
+        # If the op_code changes, reset the counter and change name on the progress bar.
+        if op_code != self.last_op_code:
+            PROGRESS_PROJECT.reset(TASK_PROJECT)
+            self.last_op_code = op_code
+
+        # Caulcate the percentage done for this op_code
+        percent = 0
+        if max_count:
+            percent = int((cur_count / max_count) * 100)
+        description = OP_CODE_MAP.get(op_code, "").title()
+        
+        # Update the project's progress bar
+        PROGRESS_PROJECT.update(TASK_PROJECT, description=description, completed=percent)
+        # Push to terminal
+        LIVE.update(rich.console.Group(TABLE, PROGRESS_PROJECT, PROGRESS_TOTAL), refresh=True)
+
 
 class GitlabGroup(BaseModel):
     """A Gitlab Group convenience class.
@@ -59,7 +83,6 @@ class GitlabGroup(BaseModel):
     Attributes:
         ...
     """
-
     gitlab_group: str
     gitlab_url: str = "https://gitlab.com"
     gitlab_key: str = os.environ.get("GITLAB_PRIVATE_KEY", "")
@@ -108,26 +131,33 @@ class GitlabGroup(BaseModel):
     def recursive_command(self, command: str, **kwargs) -> None:
         """Recursively walk down group tree, finding projects and executing commands."""
         if not self.subgroup:
-            PROGRESS.update(TASK, total=self.count)
+            PROGRESS_TOTAL.update(TASK_TOTAL, description=self.gitlab_group, total=self.count)
+        else:
+            PROGRESS_TOTAL.update(TASK_TOTAL, description=self.gitlab_group)
         
         for project in self.projects:
             if hasattr(project, command):
                 getattr(project, command)(**kwargs)
                 TABLE.add_row(*project.row)
-                PROGRESS.advance(TASK)
-                LIVE.update(rich.console.Group(TABLE, PROGRESS))
-                LIVE.refresh()
+                PROGRESS_TOTAL.update(TASK_TOTAL, description=project.name)
+                PROGRESS_TOTAL.advance(TASK_TOTAL)
+                # PROGRESS_PROJECT.update(TASK_PROJECT, total=self.count)
+                LIVE.update(rich.console.Group(TABLE, PROGRESS_PROJECT, PROGRESS_TOTAL), refresh=True)
             else:
                 raise Exception(f'Command "{command}" not recognised.')
 
         for subgroup in self.subgroups:
             subgroup.recursive_command(command) 
+        
+        if not self.subgroup:
+            LIVE.update(rich.console.Group(TABLE), refresh=True)
 
 
 class GitlabProject(BaseModel):
     """A Gitlab Project convenience class."""
 
     project: typing.Any
+    name: str = ""
     path: pathlib.Path | None = None
     git: typing.Any | None = None
     row: str = ""
@@ -140,15 +170,16 @@ class GitlabProject(BaseModel):
     def build_local_repo(self):
         """Clone/check local repo."""
         if not self.path.is_dir():
-            self.git = git.Repo.clone_from(self.project.ssh_url_to_repo, self.path)#, progress=CloneProgress())
+            self.git = git.Repo.clone_from(self.project.ssh_url_to_repo, self.path, progress=CloneProgress())
         else:
              self.git = git.Repo(self.path)
+        self.name = pathlib.Path(self.git.working_tree_dir).name
 
     def clone(self):
         """"""
         self.build_local_repo()
         self.row = [
-            pathlib.Path(self.git.working_tree_dir).name,
+            self.name,
             self.project.path_with_namespace,
             self.git.git.rev_parse("--abbrev-ref", "HEAD"),
             str(self.path),
